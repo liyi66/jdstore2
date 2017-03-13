dcd7347fdd46f972122eb886b82744bbcc1095fb
liyi <liyi.swu@gmail.com>
2017-03-13 15:40:28 +0800

    add add_to_cart button

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,19 @@
 class ProductsController < ApplicationController
 
-def index
-     @products = Product.all
-    end
-     def show
-       @product = Product.find(params[:id])
-     end
+  def index
+       @products = Product.all
+  end
 
-       def add_to_cart
-         @product = Product.find(params[:id])
-      current_cart.add_product_to_cart(@product)
-         redirect_to :back
-         flash[:notice] = “您的商品已加入购物车"
-       end
+   def show
+     @product = Product.find(params[:id])
+   end
+
+         def add_to_cart
+           @product = Product.find(params[:id])
+           current_cart.add_product_to_cart(@product)
+           redirect_to :back
+           flash[:notice] = "您的商品已加入购物车"
+         end
 
 
 
