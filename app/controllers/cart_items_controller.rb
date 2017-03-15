88b919860f2aa22637c0e3f57dbe778643adc644
liyi <liyi.swu@gmail.com>
2017-03-15 17:27:57 +0800

    cannot add item with quantity 0

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -18,6 +18,14 @@ class CartItemsController < ApplicationController
       @cart_item = @cart.cart_items.find_by(product_id: params[:id])
       @cart_item.update(cart_item_params)
 
+           flash[:notice] = "成功变更数量"
+         else
+           flash[:warning] = "数量不足以加入购物车"
+         end
+
+
+      
+
       redirect_to carts_path
     end
 
@@ -30,5 +38,5 @@ class CartItemsController < ApplicationController
 
 
 
-  
+
 end
